5efc0fe9b671eee16cb61debec083431d71b1178
ThiagoToWo <towo497@gmail.com>
2022-07-18 11:15:42 -0300

    simplificação no código de string tokenizer

--- stringtokenizer.js
+++ stringtokenizer.js
@@ -1,42 +1,26 @@
-function StringTokenizer(string, separadores) {
-	this.string = string;
-	this.letras = string.split('');
-	this.separadores = separadores;
-	this.tokens = [];
-	this.sortTokens = this.tokens.sort();
-	this.criarTokens();
-}
+function strtok(texto, separadores) {
+	const tokens = [];
+	let palavra = "";
+	let achou = false;
 
-StringTokenizer.prototype = {
-	criarTokens: function() {
-		var str = this.string;
-		var sep = this.separadores;	
-		var letras = this.letras;
-		var mark = 0;
-		
-		for (var i = 0; i < str.length; i++) {
-			for (var j = 0; j < sep.length; j++) {
-				if (letras[i] == sep.charAt(j)) {
-					letras[i] = '\0';
-					mark++;
-				}
+	for (let i = 0; i < texto.length; i++) {
+		for (let j = 0; j < separadores.length; j++) {
+			if (texto[i] == separadores[j]) {
+				achou = true;
+				break;
 			}
 		}
-		
-		var token = '';
-		j = 0;
-		
-		for (var i = 0; i <= mark; i++) {
-			for (; j < str.length; j++) {
-				if (letras[j] !=  '\0') {
-					token += letras[j];
-				} else break;
-			}
-			if (token != '') {
-				this.tokens.push(token);
-			}
-			token = '';
-			j++;
+
+		if (!achou) {
+			palavra += texto[i];
+		} else if(achou) {
+			tokens.push(palavra);
+			palavra = "";
+			achou = false;
 		}
 	}
+
+	tokens.push(palavra);
+
+	return tokens;
 }
